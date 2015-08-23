4493ea8efa0b05ad335ff2ac02847619eb5afbd2
Pavel Mikhailyuk <pnm@anahoret.com>
2015-08-23 17:52:26 +0300

    Add Book model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823144500) do
+ActiveRecord::Schema.define(version: 20150823144941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.integer  "publisher_id"
+    t.string   "title"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "books", ["publisher_id"], name: "index_books_on_publisher_id", using: :btree
 
   create_table "publishers", force: :cascade do |t|
     t.string   "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 20150823144500) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "publishers"
 end
