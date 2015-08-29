2c19fd3679d3b16e96ec3b558aee6f56b0ff2668
Pavel Mikhailyuk <pnm@anahoret.com>
2015-08-29 17:20:21 +0300

    Add validations to a Product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :shop
   belongs_to :book
+
+  validates_presence_of :shop_id, :book_id, :copies_in_stock, :copies_sold
+  validates :copies_in_stock, :copies_sold, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
