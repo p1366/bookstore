d78da953aef17458dcaed712792f2f28562ee2da
Pavel Mikhailyuk <pnm@anahoret.com>
2015-08-29 17:41:46 +0300

    Add method for endpoint2 logic implementation, version 1 - through 1 table.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,21 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :shop_id, :book_id, :copies_in_stock, :copies_sold
   validates :copies_in_stock, :copies_sold, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def sell(quantity)
+    quantity > 0 ? do_sell(quantity) : false
+  end
+
+  private
+
+  def do_sell(quantity)
+    transaction do
+      lock!
+      unless quantity > copies_in_stock
+        self.copies_in_stock -= quantity
+        self.copies_sold     += quantity
+        save!
+      end
+    end
+  end
 end
