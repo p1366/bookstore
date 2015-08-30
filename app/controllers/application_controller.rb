bcb98d18fbbf3d4e88c2e87269cc5c09e358ab99
Pavel Mikhailyuk <pnm@anahoret.com>
2015-08-30 16:05:33 +0300

    Add tests for OrdersController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render nothing: true, status: 404
+  end
 end
---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe OrdersController, type: :controller do
+  describe 'POST create' do
+    COPIES = 5
+    let(:product) { create :product, copies_in_stock: COPIES }
+
+    shared_examples 'sell copies and get status:' do |copies, status|
+      it "should return #{status} when trying to sell #{copies} from #{COPIES} copies" do
+        post :create, shop_id: product.shop_id, book_id: product.book_id, copies: copies
+        expect(response.status).to eq(status)
+      end
+    end
+
+    context 'positive' do
+      include_examples 'sell copies and get status:', 1,          201
+      include_examples 'sell copies and get status:', 2,          201
+      include_examples 'sell copies and get status:', COPIES - 1, 201
+      include_examples 'sell copies and get status:', COPIES,     201
+    end
+
+    context 'negative' do
+      shared_examples 'sell with wrong shop_id, book_id:' do |shop_id, book_id|
+        it 'should return 404 when shop_id and/or book_id are wrong' do
+          shop_id = product.shop_id if shop_id == 1
+          book_id = product.book_id if book_id == 1
+
+          post :create, shop_id: shop_id, book_id: book_id, copies: 1
+          expect(response.status).to eq(404)
+        end
+      end
+
+      include_examples 'sell with wrong shop_id, book_id:', 0, 0
+      include_examples 'sell with wrong shop_id, book_id:', 0, 1
+      include_examples 'sell with wrong shop_id, book_id:', 1, 0
+      include_examples 'sell with wrong shop_id, book_id:', -1, 1
+      include_examples 'sell with wrong shop_id, book_id:', 1, -1
+      include_examples 'sell with wrong shop_id, book_id:', 1, 111
+      include_examples 'sell with wrong shop_id, book_id:', 111, 1
+
+      include_examples 'sell copies and get status:', 0,       406
+      include_examples 'sell copies and get status:', -1,      406
+      include_examples 'sell copies and get status:', -COPIES, 406
+    end
+  end
+end
