0da510cc01460fa3c7fe71cf4c2b2bcd3910b8f8
Pavel Mikhailyuk <pnm@anahoret.com>
2015-09-03 21:52:52 +0300

    Fix using active_model_serializers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::Serialization
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render nothing: true, status: 404
   end
